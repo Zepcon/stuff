c32bd21cadc79811b31d762ba5f77149f8e4412c
Zepcon <flavioschroeder@gmx.de>
2020-04-02 21:59:16 +0200

    Major changes, kept folder management more general and added the mp3 download for new tracks. Works for singles for now

--- YouTunes.py
+++ YouTunes.py
@@ -1,42 +1,51 @@
 # coding: utf-8
 # Goal: Rename multiple mp3 files with their properties to get them ready for iTunes
 
+'''
+Idea space:
+- Ask user to delete the Cover_Images folder on end
+'''
+
 import eyed3 as d3
-import os, os.path, glob, datetime, requests, re, string, PIL.Image
+import os, os.path, glob, datetime, requests, re, string, PIL.Image, youtube_dl
 from bs4 import BeautifulSoup
 
-path = "C:\\Users\\Flavio\\Music\\Youtube\\Weiteres"
-os.chdir(path)
+# path = "C:\\Users\\Flavio\\Music\\Youtube\\Weiteres"
+# os.chdir(path)
 d3.log.setLevel("ERROR")  # So there are no warnings for non-standard genres
 
 
-def nameTracks(folder, genre="[Hip-Hop/Rap]"):
+def nameTracks(folderpath, genre="[Hip-Hop/Rap]"):
     """ Tracks are saved as "Artist - TrackName"
     Tracks: Option for same genre, all title numbers get a 1, same year, different artists, track name, album like "Track Name - Single"
     """
-    for file in glob.glob(os.path.join(folder, "*.mp3")):
-        if file.find("-") != -1:
-            trackArtist = os.path.basename(file).partition("-")[0]
-            title = os.path.basename(file).partition(" - ")[2].partition(".mp3")[0]
-            singleCover = findSingleCover(trackArtist,title)
-            audiofile = d3.load(file)
-            audiofile.tag.genre = genre
-            audiofile.tag.release_date = datetime.datetime.now().year
-            audiofile.tag.artist = trackArtist
-            audiofile.tag.track_num = 1
-            if title.find(" ft.") != -1:  # cut off features als well for the album name
-                audiofile.tag.album = title.partition(" ft.")[0] + " - Single"
+    for file in os.listdir(folderpath):
+        if file.endswith(".mp3"):
+            if file.find("-") != -1:
+                filepath = folderpath + "/" + file
+                trackArtist = file.partition("-")[0].strip()
+                title = file.partition(" - ")[2].partition(".mp3")[0].strip()
+                singleCover = findSingleCover(trackArtist, title)
+                audiofile = d3.load(filepath)
+                audiofile.tag.genre = genre
+                audiofile.tag.release_date = datetime.datetime.now().year # bug: year is shown on windows explorer but not in iTunes, same string
+                audiofile.tag.artist = trackArtist
+                audiofile.tag.track_num = 1
+                if title.find(" ft.") != -1:  # cut off features als well for the album name
+                    audiofile.tag.album = title.partition(" ft.")[0] + " - Single"
+                else:
+                    audiofile.tag.album = title + ' - Single'
+                if singleCover != "Error":
+                    audiofile.tag.images.set(3, open(singleCover, "rb").read(), "image/jpeg")
+                audiofile.tag.title = title
+                audiofile.tag.save()
+                # also rename the whole file to have just the title of the track
+                os.rename(filepath, folderpath + "/" + title + ".mp3")
             else:
-                audiofile.tag.album = title + ' - Single'
-            if singleCover != "error":
-                audiofile.tag.images.set(3, open(singleCover, "rb").read(), "image/jpeg")
-            audiofile.tag.title = title
-            audiofile.tag.save()
-            # also rename the whole file to have just the title of the track
-            os.rename(file, os.path.join(folder, title + ".mp3"))
+                print("File already formatted or not named properly! ")
         else:
-            print("File already formatted or not named properly! ")
-    print("Track naming finished! ")
+            print("File not formatted because not mp3!")
+    print("All Tracks managed! ")
 
 
 def nameAlbum(artist, album, genre="[Hip-Hop/Rap]"):
@@ -44,7 +53,7 @@ def nameAlbum(artist, album, genre="[Hip-Hop/Rap]"):
     Album: Same Interpret, Year, Genre, Album Name, Different Track Numbers
     """
     trackList = generateTracklist(artist, album)
-    cover = findAlbumCover(artist,album)
+    cover = findAlbumCover(artist, album)
     for file in glob.glob("*.mp3"):
         title = file.partition(".mp3")[0]
         audiofile = d3.load(file)
@@ -53,8 +62,7 @@ def nameAlbum(artist, album, genre="[Hip-Hop/Rap]"):
         audiofile.tag.artist = artist
         try:
             # todo: Check so machen, dass nur groß oder nur kleinschreibung angeschaut werden und Zeichen wie ' berücksichtigen
-            trackNum = trackList.index(string.capwords(title.partition(
-                " ft.")[0].partition(" feat.")[0])) + 1  # automation of track numbers
+            trackNum = trackList.index(string.capwords(title.partition(" ft.")[0].partition(" feat.")[0])) + 1  # automation of track numbers
             audiofile.tag.track_num = trackNum
         except:
             print("Error occured, track has to be numbered manually")
@@ -125,10 +133,12 @@ def findSingleCover(artist, single):
     Using genius.com to find the song cover to given Artist and song
     """
     base = "https://genius.com/"
-    url = base + artist.replace(" ", "-") + "-" + \
-        single.replace(" ", "-") + "-lyrics"
+    url = base + artist.replace(" ", "-") + "-" + single.replace(" ", "-") + "-lyrics"
     raw = requests.get(url)
-    imagePath = "C:/Users/Flavio/Music/Youtube/CoverTemp/"
+    # imagePath = "C:/Users/Flavio/Music/Youtube/CoverTemp/"
+    imagePath = getcwdFormat() + "/" + "Cover_Images/"
+    if not os.path.exists("Cover_Images"):
+        os.mkdir("Cover_Images")
     soup = BeautifulSoup(raw.text, "html.parser")
     try:
         imageURL = soup.findAll(class_="cover_art-image")[0]["src"]
@@ -143,28 +153,87 @@ def findSingleCover(artist, single):
                 if not block:
                     break
                 outfile.write(block)
-        print("Cover found! Resolution is: " +
-              str(PIL.Image.open(imagePath + filename).size))
+        print("Cover found!")
         return imagePath + filename
     except:
         print("Error, cover not found")
         return "Error"
 
 
+# Download lsit of URLs
+def downLoadTracks(trackList, folder=""):
+    ydl_opts = {
+        'format': 'bestaudio',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    for i in range(len(trackList)):
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            try:
+                # result = ydl.extract_info("{}".format(trackList[i]))
+                # filename = ydl.prepare_filename(result)
+                ydl.download([trackList[i]])
+            except:
+                print("Could not download track!")
+                pass
+    for file in (os.listdir(getcwdFormat())):
+        if file.endswith(".mp3"):
+            os.rename(getcwdFormat() + "/" + file, getcwdFormat() + "/" + folder + "/" + renameDownloadTrack(file))
+
+
+# Name to "Artist - Trackname"
+def renameDownloadTrack(trackName):
+    trackName = trackName[0:trackName.rindex("-")]
+    return trackName + ".mp3"
+
+
+# Make os path suitable for python chdir
+def pathReplace(path):
+    path = path.replace("\\", "/")
+    return path
+
+
+def getcwdFormat():
+    cwd = os.getcwd().replace("\\", "/")
+    return cwd
+
+
 # Mainloop
 while True:
-    question = input('Album or Tracks? ')
+    question = input("Download Tracks or album? ")
     # name a couple of tracks
     if question in ["Tracks", "Track", "t", "T", "tr"]:
-        os.chdir("C:\\Users\\Flavio\\Music\\Youtube\\Weiteres")
-        while True:
-            foldername = input("Enter folder name: ")
-            if os.path.exists(foldername):
-                print("Folder found!")
-                nameTracks(foldername)
-                break
-            else:
-                print("Folder not found")
+        folderName = "Singles - " + str(datetime.date.today())
+        folderPath = getcwdFormat() + "/" + folderName
+        if not os.path.exists(folderName):
+            os.mkdir(folderName)
+        track_urls = []
+        question = input("Enter a song url or \"finish\": ")
+        while question not in ["f", "finished", "fi", "finish"]:
+            track_urls.append(question)
+            question = input("Enter a song url or \"finish\": ")
+        downLoadTracks(track_urls, "Singles - " + str(datetime.date.today()))
+        print("Make sure every Track is named like Artist - TrackName Features")
+        print("Example: Drake - Sneakin feat. 21 Savage")
+        print("If Track has correct name just press enter, otherwise enter correct name and then enter")
+        for file in (os.listdir(folderPath)):
+            if file.endswith(".mp3"):
+                print(file)
+                newname = input("Enter or new name: ")
+                if newname == "":
+                    pass
+                else:
+                    os.rename(getcwdFormat() + "/" + folderName + "/" + file, getcwdFormat() + "/" + folderName + "/" + newname + ".mp3")
+                    print("Saved new name!")
+        print("Every file in folder " + folderName + " has been named.")
+        print("Next up: Setting the stats for iTunes")  # todo Manage different genres here
+        nameTracks(folderPath)
+
+
+
     # name an album
     elif question in ["Album", "a", "A", "al"]:
         os.chdir("C:\\Users\\Flavio\\Music\\Youtube")
